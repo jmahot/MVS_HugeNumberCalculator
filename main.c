20ac14e1d158dd3f208aea5243b65161ad12d19f
JeanMichMich01 <104188157+JeanMichMich01@users.noreply.github.com>
2022-05-26 15:09:23 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,1 @@
-int main (void) {
-    HugeUnsignedInt* hugeNumber = createHugeUnsignedIntFromString ("123456789012345678901234567890");
-    printHugeUnsignedInt (hugeNumber);
-    hugeNumber = createHugeUnsignedIntFromString ("0000000123456789012345678901234567890");
-    printHugeUnsignedInt (hugeNumber);
-}
+
